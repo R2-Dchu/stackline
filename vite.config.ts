2bb74a7f6ebf1e3f7cba7b2a1184f217e07928c6
R2-Dchu <darrenchu650@gmail.com>
2025-01-11 18:37:24 -0800

    Fix Base

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/stackline",
+  base:"/stackline/",
   server: {
     open: true,
   },
